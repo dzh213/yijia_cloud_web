af5309075d57dedbb0d7ea8517e0bc9cd547982a
donghao <dzh213@163.com>
2019-07-02 22:43:23 +0800

    解决ajax aop问题

--- plugin/ajaxaop.js
+++ plugin/ajaxaop.js
@@ -2,16 +2,42 @@ layui.define('ajaxhook',function (exports) {
     hook = hookAjax({
         //拦截回调
         onreadystatechange: function (xhr) {
-            console.log("onreadystatechange called: %O", xhr)
+            var access_token = xhr.getResponseHeader("access_token")
+            if(access_token != "") {
+                setCookie("access_token",access_token,15);                
+            }
         },
         onload: function (xhr) {
-            console.log("onload called: %O", xhr)
+           
         },
         //拦截方法
         open: function (arg, xhr) {
-            console.log("open called: method:%s,url:%s,async:%s", arg[0], arg[1], arg[2])
+              
+        },
+        send: function(arg,xhr) {
+            xhr.setRequestHeader("contentType","application/json; charset=utf-8");
+            var access_token = getCookie("access_token");
+            if(access_token != "") {
+                xhr.setRequestHeader("access_token", access_token)
+            }
         }
     })
+
+    function setCookie(cname,cvalue,exdays){
+        var d = new Date();
+        d.setTime(d.getTime()+(exdays*24*60*60*1000));
+        var expires = "expires="+d.toGMTString();
+        document.cookie = cname+"="+cvalue+"; "+expires;
+    }
+    function getCookie(cname){
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+        }
+        return "";
+    }
 
     //对外输出
     exports('ajaxaop', hook);
